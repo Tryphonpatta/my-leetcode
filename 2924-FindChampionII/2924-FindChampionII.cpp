bd48ec7315eb42734874067973da180038f580f5
Patthadon Tantiameorn <beebobbob956@gmail.com>
2024-11-26 10:00:36 +0700

    Update file 11/26/2024, 10:00:36 AM

--- 2924-FindChampionII/2924-FindChampionII.cpp
+++ 2924-FindChampionII/2924-FindChampionII.cpp
@@ -2,7 +2,6 @@ class Solution {
 public:
     int findChampion(int n, vector<vector<int>>& edges) {
         int degreeIn[101] = {};
-        int degreeOut[101] = {};
         for(auto edge : edges) {
             degreeIn[edge[1]]++;
         }
@@ -10,10 +9,10 @@ public:
         for(int i = 0; i < n ; i++){
             // cout << degreeIn[i] << endl;
             if(degreeIn[i] == 0){
-                if(ans == -1){
-                    ans = i;
+                if(ans != -1){
+                    return -1;
                 }
-                else return -1;
+                ans = i;
             }
         }
         return ans;
