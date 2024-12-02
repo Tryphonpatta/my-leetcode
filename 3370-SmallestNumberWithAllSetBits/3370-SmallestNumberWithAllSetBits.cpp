da5487f6edc7d1b419a9d1cf2e52aa7a0407b545
Patthadon Tantiameorn <beebobbob956@gmail.com>
2024-12-02 10:36:30 +0700

    Update file 12/2/2024, 10:36:30 AM

--- 3370-SmallestNumberWithAllSetBits/3370-SmallestNumberWithAllSetBits.cpp
+++ 3370-SmallestNumberWithAllSetBits/3370-SmallestNumberWithAllSetBits.cpp
@@ -1,12 +1,23 @@
 class Solution {
 public:
-    int smallestNumber(int n) {
-        int exp = 2;
-        while(1) {
-            if(n <= exp - 1)
-                return exp -1;
-            exp *= 2;
+    int getLargestOutlier(vector<int>& nums) {
+        int sum= 0;
+        int ans = INT_MIN;
+        map<int,int> m;
+        for(auto num : nums) {
+            sum += num;
+            m[num] += 1;
         }
-        return -1;
+        for(auto num : nums){
+            if((sum - num) % 2 == 0){
+                int cnt = m[(sum - num) / 2] - (((sum - num) / 2) == num);
+                if(cnt){
+                    // cout << cnt << endl;
+                    // cout<< num << " " << (sum - num) / 2 << endl;
+                    ans = max(ans, num);
+                }
+            }
+        }
+        return ans;
     }
 };
