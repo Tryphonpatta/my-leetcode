b25400eb92071f30da54197b44664803322bf833
Patthadon Tantiameorn <beebobbob956@gmail.com>
2025-03-02 17:59:14 +0700

    File updated at 3/2/2025, 5:59:13 PM

--- 2570-MergeTwo2DArraysbySummingValues/2570-MergeTwo2DArraysbySummingValues.cpp
+++ 2570-MergeTwo2DArraysbySummingValues/2570-MergeTwo2DArraysbySummingValues.cpp
@@ -1,32 +1,33 @@
 class Solution {
 public:
     vector<vector<int>> mergeArrays(vector<vector<int>>& nums1, vector<vector<int>>& nums2) {
-        vector<vector<int>> ans;
-        int i = 0 ;
-        int j = 0;
-        while(i < nums1.size() && j < nums2.size()){
-            if (nums1[i][0] == nums2[j][0]){
-                ans.push_back({nums1[i][0],nums1[i][1] + nums2[j][1]});
-                j++;
-                i++;
+        vector<vector<int>>ans;
+        int temp1=0, temp2=0;
+        int n1 = nums1.size(), n2 = nums2.size();
+        while(temp1<n1 && temp2<n2){
+            if(nums1[temp1][0]==nums2[temp2][0]){
+                ans.push_back({nums1[temp1][0],nums1[temp1][1]+nums2[temp2][1]});
+                temp1++;temp2++;
             }
-            else if(nums1[i][0] < nums2[j][0]){
-                ans.push_back({nums1[i][0],nums1[i][1]});
-                i ++;
+            else if(nums1[temp1][0]<nums2[temp2][0]){
+                ans.push_back({nums1[temp1][0],nums1[temp1][1]});
+                temp1++;               
             }
-            else {
-                ans.push_back({nums2[j][0],nums2[j][1]});
-                j++;
+            else{
+                ans.push_back({nums2[temp2][0],nums2[temp2][1]});
+                temp2++;                
             }
         }
-        while(i < nums1.size()){
-            ans.push_back({nums1[i][0],nums1[i][1]});
-            i ++;
+
+        while(temp1<n1){
+            ans.push_back({nums1[temp1][0],nums1[temp1][1]});
+            temp1++; 
         }
-        while(j < nums2.size()){
-            ans.push_back({nums2[j][0],nums2[j][1]});
-            j ++;
+
+        while(temp2<n2){
+            ans.push_back({nums2[temp2][0],nums2[temp2][1]});
+            temp2++; 
         }
-        return ans;
+        return ans; 
     }
 };
