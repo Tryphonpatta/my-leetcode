63872ab270f61c36d2483a99b7a94750b5e0e7b1
Patthadon Tantiameorn <beebobbob956@gmail.com>
2025-04-17 12:19:33 +0700

    Create file

---
+++ 2176-CountEqualandDivisiblePairsinanArray/2176-CountEqualandDivisiblePairsinanArray.cpp
@@ -0,0 +1,14 @@
+// Last updated: 4/17/2025, 12:19:32 PM
+class Solution {
+public:
+    int countPairs(vector<int>& nums, int k) {
+        int ans  = 0;
+        for(int i = 0; i < nums.size() ; i++) {
+            for(int j = i + 1 ; j < nums.size() ;j++){
+                if(nums[i] == nums[j] && (i * j) % k == 0 )ans ++;
+            }
+        }
+        return ans;
+
+    }
+};
