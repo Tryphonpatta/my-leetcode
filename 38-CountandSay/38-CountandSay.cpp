17fc36360826034e7a490bd7b4e3e0748942fd3a
Patthadon Tantiameorn <beebobbob956@gmail.com>
2025-04-18 09:28:20 +0700

    File updated at 4/18/2025, 9:28:20 AM

--- 38-CountandSay/38-CountandSay.cpp
+++ 38-CountandSay/38-CountandSay.cpp
@@ -1,35 +1,26 @@
-// Last updated: 4/18/2025, 9:28:00 AM
+// Last updated: 4/18/2025, 9:28:20 AM
 class Solution {
 public:
-    vector<vector<int>> stoa(string s){
-        int cnt = 1;
-        vector<vector<int>> v;
-        for(int i = 0 ; i < s.size() ; i++){
-            if(i + 1 >= s.size() || s[i] != s[i + 1]){
-                v.push_back({cnt,s[i]});
-                cnt = 1;
-            }
-            else cnt ++;
-        }
-        return v;
-    }
-    string atos(vector<vector<int>> v){
-        string ans = "";
-        for(int i = 0 ; i < v.size() ; i++){
-            // cout << v[i][0] << ' ' << v[i][1] << endl;
-            ans += char(v[i][0] + '0');
-            ans += char(v[i][1] );
-        }
-        // cout << ans << endl;
-        // cout << endl;
-        return ans;
-    }
     string countAndSay(int n) {
-        string ans = "1";
-        for(int i = 1; i < n ; i ++){
-            vector<vector<int>> x = stoa(ans);
-            ans = atos(x);
+        if(n==1) return "1";
+        
+        string str = countAndSay(n-1);
+        
+        string res = "";
+        int s = str.size();
+        int i = 0;
+        while(i<s)
+        {
+            char c = str[i];
+            int count = 0;
+            while(i<s and str[i]==c)
+            {
+                count++;
+                i++;
+            }
+            res+='0'+count;
+            res+=c;
         }
-        return ans;
+        return res;
     }
 };
