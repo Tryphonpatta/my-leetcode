450a541fc9dd9bf5be9a0ebac70907e86b06709a
Patthadon Tantiameorn <beebobbob956@gmail.com>
2025-04-21 14:07:36 +0700

    Create file

---
+++ 330-PatchingArray/330-PatchingArray.cpp
@@ -0,0 +1,22 @@
+// Last updated: 4/21/2025, 2:07:36 PM
+class Solution {
+public:
+    int minPatches(vector<int>& nums, int n) {
+        long long miss = 1;
+        int result = 0;
+        int i = 0;
+        while(miss <= n) {
+            
+            if (i < nums.size() && nums[i] <= miss) {
+                miss += nums[i];
+                i ++;
+            }
+            else {
+                miss += miss;
+                result ++;
+            }
+            cout << miss << endl;
+        }
+        return result;
+    }
+};
