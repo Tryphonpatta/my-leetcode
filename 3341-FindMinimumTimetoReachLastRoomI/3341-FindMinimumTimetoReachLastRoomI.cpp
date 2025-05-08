6de8a2630e86e145e67a5a9da96c868f54fe128c
Patthadon Tantiameorn <beebobbob956@gmail.com>
2025-05-08 11:26:35 +0700

    File updated at 5/8/2025, 11:26:34 AM

--- 3341-FindMinimumTimetoReachLastRoomI/3341-FindMinimumTimetoReachLastRoomI.cpp
+++ 3341-FindMinimumTimetoReachLastRoomI/3341-FindMinimumTimetoReachLastRoomI.cpp
@@ -1,13 +1,14 @@
-// Last updated: 5/7/2025, 3:33:55 PM
+// Last updated: 5/8/2025, 11:26:34 AM
 class Solution {
 public:
     int minTimeToReach(vector<vector<int>>& moveTime) {
-        priority_queue<pair<int,pair<int,int>>,vector<pair<int,pair<int,int>>>,greater<pair<int,pair<int,int>>> > pq;
-        pq.push({0,{0,0}});
+        priority_queue<pair<pair<int,int>,pair<int,int>>,vector<pair<pair<int,int>,pair<int,int>>>,greater<pair<pair<int,int>,pair<int,int>>> > pq;
+        pq.push({{0,1},{0,0}});
         int di[4][2] = {{0,1},{0,-1},{1,0},{-1,0}};
-        bool vis[51][51] = {};
+        bool vis[751][751] = {};
         while(!pq.empty()){
-            int t = pq.top().first;
+            int t = pq.top().first.first;
+            int time = pq.top().first.second;
             int x = pq.top().second.first;
             int y = pq.top().second.second;
             pq.pop();
@@ -20,7 +21,7 @@ public:
                 int newx = x + di[i][0];
                 int newy = y + di[i][1];
                 if(newx < 0 || newy < 0 || newx >= moveTime.size() || newy >= moveTime[0].size())continue;
-                pq.push({max(t + 1, moveTime[newx][newy] + 1), {newx,newy}});
+                pq.push({{max(t + time, moveTime[newx][newy] + time),time == 1 ? 2 : 1}, {newx,newy}});
             }
         }
         return -1;
