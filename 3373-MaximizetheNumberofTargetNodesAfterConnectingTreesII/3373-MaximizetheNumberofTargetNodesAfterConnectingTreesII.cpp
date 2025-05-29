31c5fb13848b0ec79b24993ed0e4a9d71d301a75
Patthadon Tantiameorn <beebobbob956@gmail.com>
2025-05-29 20:18:34 +0700

    File updated at 5/29/2025, 8:18:34 PM

--- 3373-MaximizetheNumberofTargetNodesAfterConnectingTreesII/3373-MaximizetheNumberofTargetNodesAfterConnectingTreesII.cpp
+++ 3373-MaximizetheNumberofTargetNodesAfterConnectingTreesII/3373-MaximizetheNumberofTargetNodesAfterConnectingTreesII.cpp
@@ -1,62 +1,41 @@
-// Last updated: 5/29/2025, 8:17:09 PM
+// Last updated: 5/29/2025, 8:18:34 PM
 class Solution {
 public:
-    vector<int> maxTargetNodes(vector<vector<int>>& edges1, vector<vector<int>>& edges2) {
-        vector<int> v1[100001];
-        vector<int> v2[100001];
-        for(auto e : edges1) {
-            v1[e[0]].push_back(e[1]);
-            v1[e[1]].push_back(e[0]);
-        }
-        for(auto e : edges2) {
-            v2[e[0]].push_back(e[1]);
-            v2[e[1]].push_back(e[0]);
-        }
-        queue<int> q;
-        int c =0;
-        int cnt1[2] = { };
-        int cnt2[2] = {};
-        int a1[100001] = {};
-        bool vis1[100001] = {},vis2[100001]= {};
-        q.push(0);
-        while(!q.empty()){
-            int n = q.size();
-            for(int i = 0; i < n ; i ++) {
-                int x= q.front();
-                q.pop();
-                cnt1[c % 2] ++;
-                a1[x] = c % 2;
-                vis1[x] = true;
-                for(auto j : v1[x]){
-                    if(vis1[j])continue;
-                    q.push(j);
-                }
+    int dfs(int node, int parent, int depth,
+            const vector<vector<int>>& children, vector<int>& color) {
+        int res = 1 - depth % 2;
+        color[node] = depth % 2;
+        for (int child : children[node]) {
+            if (child == parent) {
+                continue;
             }
-            c++;
+            res += dfs(child, node, depth + 1, children, color);
         }
-        int m = 0;
-        q.push(0);
-        c = 0;
-        while(!q.empty()){
-            int n = q.size();
-            for(int i = 0; i < n ; i ++) {
-                int x= q.front();
-                q.pop();
-                cnt2[c % 2] ++;
-                m = max(cnt2[c%2],m);
-                vis2[x] = true;
-                for(auto j : v2[x]){
-                    if(vis2[j])continue;
-                    q.push(j);
-                }
-            }
-            c++;
+        return res;
+    }
+
+    vector<int> build(const vector<vector<int>>& edges, vector<int>& color) {
+        int n = edges.size() + 1;
+        vector<vector<int>> children(n);
+        for (const auto& edge : edges) {
+            children[edge[0]].push_back(edge[1]);
+            children[edge[1]].push_back(edge[0]);
         }
-        vector<int> ans;
-        for(int i =0; i <= edges1.size();i++){
-            // cout << i << " " << cnt1[a1[i]] << endl;
-            ans.push_back(cnt1[a1[i]] + m);
+        int res = dfs(0, -1, 0, children, color);
+        return {res, n - res};
+    }
+
+    vector<int> maxTargetNodes(vector<vector<int>>& edges1,
+                               vector<vector<int>>& edges2) {
+        int n = edges1.size() + 1, m = edges2.size() + 1;
+        vector<int> color1(n);
+        vector<int> color2(m);
+        vector<int> count1 = build(edges1, color1);
+        vector<int> count2 = build(edges2, color2);
+        vector<int> res(edges1.size() + 1);
+        for (int i = 0; i < n; i++) {
+            res[i] = count1[color1[i]] + max(count2[0], count2[1]);
         }
-        return ans;
+        return res;
     }
 };
