39fbb1ace94ff158109d9f21db6cd4c7dff20c2a
Patthadon Tantiameorn <beebobbob956@gmail.com>
2025-05-30 13:58:48 +0700

    File updated at 5/30/2025, 1:58:48 PM

--- 2359-FindClosestNodetoGivenTwoNodes/2359-FindClosestNodetoGivenTwoNodes.cpp
+++ 2359-FindClosestNodetoGivenTwoNodes/2359-FindClosestNodetoGivenTwoNodes.cpp
@@ -1,47 +1,30 @@
-// Last updated: 5/30/2025, 1:58:34 PM
+// Last updated: 5/30/2025, 1:58:48 PM
 class Solution {
 public:
     int closestMeetingNode(vector<int>& edges, int node1, int node2) {
-        int vis1[100001] = {};
-        memset(vis1,-1,sizeof(vis1));
-        queue<pair<int, int>> q;
-        q.push({node1, 0});
-        while (!q.empty()) {
-            int x = q.front().first;
-            int y = q.front().second;
-            q.pop();
-            if (vis1[x] != -1)
-                continue;
-            vis1[x] = y;
-            if (edges[x] != -1) {
-                q.push({edges[x], y + 1});
-            }
+        int n = edges.size();
+        vector<int> visited(n,-1), visited_2(n,-1);
+        int depth = 0;
+        while(node1 != -1 && visited[node1] == -1){
+            visited[node1] = depth;
+            node1 = edges[node1]; 
+            depth++;
         }
-        int vis2[100001] = {};
-        memset(vis2,-1,sizeof(vis2));
-        q.push({node2, 0});
-        while (!q.empty()) {
-            int x = q.front().first;
-            int y = q.front().second;
-            q.pop();
-            if (vis2[x] != -1)
-                continue;
-            vis2[x] = y;
-            if (edges[x] != -1) {
-                q.push({edges[x], y + 1});
-            }
-        }
-        int ans = -1;
-        int max_dis = INT_MAX;
-        for(int i = 0; i < edges.size() ; i++){
-            if(vis1[i] != -1 && vis2[i] != -1) {
-                int x = max(vis1[i],vis2[i]);
-                if(x < max_dis) {
-                    max_dis = x;
-                    ans = i;
+        int ans = INT_MAX, idx = -1;
+        depth = 0;
+        while(node2 != -1 && visited_2[node2] == -1){
+            visited_2[node2] = depth;
+            if(visited[node2] != -1){
+                if(max(visited[node2], depth) < ans){
+                    ans = max(visited[node2], depth);
+                    idx = node2;
+                }else if(max(visited[node2], depth) == ans){
+                    if(idx > node2) idx = node2;
                 }
             }
+            node2 = edges[node2];
+            depth++;
         }
-        return ans;
+        return idx;
     }
 };
