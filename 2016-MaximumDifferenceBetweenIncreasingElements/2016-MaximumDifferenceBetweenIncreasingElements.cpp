2025a8af4832e3a5015c242e146ef655ab111a3f
Patthadon Tantiameorn <beebobbob956@gmail.com>
2025-06-16 14:45:59 +0700

    Create 2016-MaximumDifferenceBetweenIncreasingElements

---
+++ 2016-MaximumDifferenceBetweenIncreasingElements/2016-MaximumDifferenceBetweenIncreasingElements.cpp
@@ -0,0 +1,15 @@
+// Last updated: 6/16/2025, 2:45:59 PM
+class Solution {
+public:
+    int maximumDifference(vector<int>& nums) {
+        int ans =-1;
+        for(int i = 0; i< nums.size() ;i ++){
+            for(int j = i + 1;j < nums.size() ;j ++){
+                if(nums[i] < nums[j]) {
+                    ans = max(ans, abs(nums[i] - nums[j]));
+                }
+            }
+        }
+        return ans;
+    }
+};
