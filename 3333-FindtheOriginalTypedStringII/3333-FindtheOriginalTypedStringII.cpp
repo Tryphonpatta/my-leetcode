b51cf13f31e5403aabd132e0e6d2200b3cdb7141
Patthadon Tantiameorn <beebobbob956@gmail.com>
2025-07-02 20:45:48 +0700

    File updated at 7/2/2025, 8:45:48 PM

--- 3333-FindtheOriginalTypedStringII/3333-FindtheOriginalTypedStringII.cpp
+++ 3333-FindtheOriginalTypedStringII/3333-FindtheOriginalTypedStringII.cpp
@@ -1,13 +1,49 @@
-// Last updated: 5/2/2025, 9:34:40 AM
+// Last updated: 7/2/2025, 8:45:48 PM
 class Solution {
 public:
-    int minimizeArrayValue(vector<int>& nums) {
-        long long ans = 0;
-        long long prefix = 0;
-        for(int i = 0 ; i < nums.size() ; i++){
-            prefix += nums[i];
-            ans = max(ans, (prefix + i) / (i + 1));
+    int possibleStringCount(string word, int k) {
+        int n = word.size(), cnt = 1;
+        vector<int> freq;
+        for (int i = 1; i < n; ++i) {
+            if (word[i] == word[i - 1]) {
+                ++cnt;
+            } else {
+                freq.push_back(cnt);
+                cnt = 1;
+            }
         }
-        return ans;
+        freq.push_back(cnt);
+
+        int ans = 1;
+        for (int o : freq) {
+            ans = static_cast<long long>(ans) * o % mod;
+        }
+
+        if (freq.size() >= k) {
+            return ans;
+        }
+
+        vector<int> f(k), g(k, 1);
+        f[0] = 1;
+        for (int i = 0; i < freq.size(); ++i) {
+            vector<int> f_new(k);
+            for (int j = 1; j < k; ++j) {
+                f_new[j] = g[j - 1];
+                if (j - freq[i] - 1 >= 0) {
+                    f_new[j] = (f_new[j] - g[j - freq[i] - 1] + mod) % mod;
+                }
+            }
+            vector<int> g_new(k);
+            g_new[0] = f_new[0];
+            for (int j = 1; j < k; ++j) {
+                g_new[j] = (g_new[j - 1] + f_new[j]) % mod;
+            }
+            f = move(f_new);
+            g = move(g_new);
+        }
+        return (ans - g[k - 1] + mod) % mod;
     }
+
+private:
+    static const int mod = 1000000007;
 };
