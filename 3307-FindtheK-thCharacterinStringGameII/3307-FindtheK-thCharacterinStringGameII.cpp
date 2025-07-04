2a5a21377a7e238bef136ac2f04e573c2c0819e1
Patthadon Tantiameorn <beebobbob956@gmail.com>
2025-07-04 20:49:18 +0700

    Create 3307-FindtheK-thCharacterinStringGameII

---
+++ 3307-FindtheK-thCharacterinStringGameII/3307-FindtheK-thCharacterinStringGameII.cpp
@@ -0,0 +1,19 @@
+// Last updated: 7/4/2025, 8:49:17 PM
+class Solution {
+public:
+    char kthCharacter(long long k, vector<int>& operations) {
+        int ans = 0;
+        int t;
+        while (k != 1) {
+            t = __lg(k);
+            if (((long long)1 << t) == k) {
+                t--;
+            }
+            k = k - ((long long)1 << t);
+            if (operations[t]) {
+                ans++;
+            }
+        }
+        return 'a' + (ans % 26);
+    }
+};
