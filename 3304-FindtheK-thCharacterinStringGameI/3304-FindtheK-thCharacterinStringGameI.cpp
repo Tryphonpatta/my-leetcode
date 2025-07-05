8e48a3cf1505cdf6c18c01398aa57e0afd3e8868
Patthadon Tantiameorn <beebobbob956@gmail.com>
2025-07-05 11:14:05 +0700

    Create 3304-FindtheK-thCharacterinStringGameI

---
+++ 3304-FindtheK-thCharacterinStringGameI/3304-FindtheK-thCharacterinStringGameI.cpp
@@ -0,0 +1,17 @@
+// Last updated: 7/5/2025, 11:14:05 AM
+class Solution {
+public:
+    char kthCharacter(int k) {
+        int ans = 0;
+        int t;
+        while (k != 1) {
+            t = __lg(k);
+            if ((1 << t) == k) {
+                t--;
+            }
+            k = k - (1 << t);
+            ans++;
+        }
+        return 'a' + ans;
+    }
+};
