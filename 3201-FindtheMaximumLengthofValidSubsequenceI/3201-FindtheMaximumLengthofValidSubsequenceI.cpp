e93b34c7d1a67b09a8754f112b8b89d4fc1ebbfa
Patthadon Tantiameorn <beebobbob956@gmail.com>
2025-07-16 13:23:24 +0700

    Create 3201-FindtheMaximumLengthofValidSubsequenceI

---
+++ 3201-FindtheMaximumLengthofValidSubsequenceI/3201-FindtheMaximumLengthofValidSubsequenceI.cpp
@@ -0,0 +1,18 @@
+// Last updated: 7/16/2025, 1:23:23 PM
+class Solution {
+public:
+    int maximumLength(vector<int>& nums) {
+        int res = 0;
+        vector<vector<int>> patterns = {{0, 0}, {0, 1}, {1, 0}, {1, 1}};
+        for (auto& pattern : patterns) {
+            int cnt = 0;
+            for (int num : nums) {
+                if (num % 2 == pattern[cnt % 2]) {
+                    cnt++;
+                }
+            }
+            res = max(res, cnt);
+        }
+        return res;
+    }
+};
