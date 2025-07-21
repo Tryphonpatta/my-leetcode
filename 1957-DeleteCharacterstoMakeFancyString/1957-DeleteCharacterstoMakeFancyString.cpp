e8d29bbf95b5c9974a587d94d582636e48016059
Patthadon Tantiameorn <beebobbob956@gmail.com>
2025-07-22 00:23:50 +0700

    File updated at 7/22/2025, 12:23:49 AM

--- 1957-DeleteCharacterstoMakeFancyString/1957-DeleteCharacterstoMakeFancyString.cpp
+++ 1957-DeleteCharacterstoMakeFancyString/1957-DeleteCharacterstoMakeFancyString.cpp
@@ -1,18 +1,19 @@
+// Last updated: 7/22/2025, 12:23:49 AM
 class Solution {
 public:
     string makeFancyString(string s) {
-        stringstream ss;
-        ss << s[0];
-
-        int count = 1;
-        for(int i=1; i<s.size(); i++){
-            if(s[i] == s[i-1]) count++;
-            else count = 1;
-
-            if(count >= 3) continue;
-            ss << s[i];
+        string ans = "" ;
+        int cnt = 1;
+        ans.push_back(s[0]);
+        for(int i = 1; i < s.size() ; i++){
+            if(s[i] == s[i - 1]){
+                cnt ++;
+            }
+            else {
+                cnt = 1;
+            }
+            if(cnt < 3)ans.push_back(s[i]);
         }
-
-        return ss.str();
+        return ans;
     }
 };
