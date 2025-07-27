893d8925a5e726aa74624ae5996ff9d75585ae1b
Patthadon Tantiameorn <beebobbob956@gmail.com>
2025-07-28 01:01:24 +0700

    Create 2210-CountHillsandValleysinanArray

---
+++ 2210-CountHillsandValleysinanArray/2210-CountHillsandValleysinanArray.cpp
@@ -0,0 +1,20 @@
+// Last updated: 7/28/2025, 1:01:23 AM
+class Solution {
+public:
+    int countHillValley(std::vector<int>& nums) {
+        int count = 0;
+        int left = 0;
+
+        for (int i = 1; i < nums.size() - 1; ++i) {
+            if (nums[i] != nums[i + 1]) {
+                if ((nums[i] > nums[left] && nums[i] > nums[i + 1]) ||
+                    (nums[i] < nums[left] && nums[i] < nums[i + 1])) {
+                    ++count;
+                }
+                left = i;
+            }
+        }
+
+        return count;
+    }
+};
